88e28ff049a972d26f4d7f740e20b342538328ea
Brian Harrington <brharrington@gmail.com>
2014-09-24 13:58:43 -0700

    fix deprecation warnings for ribbon

--- helloworld-servlet/src/main/java/com/netflix/helloworld/servlet/EddaServlet.java
+++ helloworld-servlet/src/main/java/com/netflix/helloworld/servlet/EddaServlet.java
@@ -1,10 +1,12 @@
 package com.netflix.helloworld.servlet;
 
-import com.netflix.client.ClientFactory;
-import com.netflix.client.http.HttpRequest;
-import com.netflix.client.http.HttpResponse;
-import com.netflix.discovery.DiscoveryManager;
-import com.netflix.niws.client.http.RestClient;
+import com.netflix.client.config.DefaultClientConfigImpl;
+import com.netflix.client.config.IClientConfig;
+import com.netflix.ribbon.transport.netty.RibbonTransport;
+import com.netflix.ribbon.transport.netty.http.LoadBalancingHttpClient;
+import io.netty.buffer.ByteBuf;
+import io.reactivex.netty.protocol.http.client.HttpClientRequest;
+import io.reactivex.netty.protocol.http.client.HttpClientResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,7 +16,6 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.URI;
 
 /**
  * Return the edda content for this instance.
@@ -26,6 +27,14 @@ public class EddaServlet extends BaseServlet {
 
   private static final String EDDA_CLIENT_NAME = "edda";
   private static final String EDDA_PATH = "/api/v2/view/instances/%s";
+
+  private final LoadBalancingHttpClient<ByteBuf, ByteBuf> eddaClient = createEddaClient();
+
+  private LoadBalancingHttpClient<ByteBuf, ByteBuf> createEddaClient() {
+    final IClientConfig config =
+        DefaultClientConfigImpl.getClientConfigWithDefaultValues(EDDA_CLIENT_NAME);
+    return RibbonTransport.newHttpClient(config);
+  }
 
   private String getInstanceId() {
     return System.getenv("EC2_INSTANCE_ID");
@@ -39,29 +48,25 @@ public class EddaServlet extends BaseServlet {
     }
   }
 
-  @SuppressWarnings("deprecation")
-  @Override public void doGet(HttpServletRequest req, HttpServletResponse res) {
+  @Override public void doGet(final HttpServletRequest req, final HttpServletResponse res) {
     String id = getInstanceId();
     if (id == null) {
       res.setStatus(500);
     } else {
       try {
-        RestClient rc = (RestClient) ClientFactory.getNamedClient(EDDA_CLIENT_NAME);
-        HttpRequest eddaReq = new HttpRequest.Builder()
-            .verb(HttpRequest.Verb.GET)
-            .uri(String.format(EDDA_PATH, id))
-            .build();
-        try (HttpResponse eddaRes = rc.executeWithLoadBalancer(eddaReq)) {
-          if (eddaRes.getStatus() == 200) {
-            res.setStatus(200);
-            res.setContentType("application/json");
-            try (
-                InputStream in = eddaRes.getInputStream();
-                OutputStream out = res.getOutputStream()) {
-              copy(in, out);
-            }
+        HttpClientRequest<ByteBuf> eddaReq =
+            HttpClientRequest.createGet(String.format(EDDA_PATH, id));
+        HttpClientResponse<ByteBuf> eddaRes = eddaClient.submit(eddaReq)
+            .toBlocking()
+            .single();
+        res.setStatus(eddaRes.getStatus().code());
+        eddaRes.getContent().toBlocking().forEach((ByteBuf content) -> {
+          try {
+            content.readBytes(res.getOutputStream(), content.capacity());
+          } catch (IOException e) {
+            LOGGER.error("failed to write edda output", e);
           }
-        }
+        });
       } catch (Exception e) {
         LOGGER.error("failed to get data from edda", e);
         res.setStatus(500);
